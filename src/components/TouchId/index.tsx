f0adf3e902bfe0686253c387fa63375efac03183
2vm <vladimir.matsola@litslink.com>
2018-02-22 18:49:11 +0200

    test1

--- src/components/TouchId/index.tsx
+++ src/components/TouchId/index.tsx
@@ -18,48 +18,49 @@ class TouchId extends React.Component<ITouchIdProps> {
   render() {
     console.log(Platform, 'plf')
     return (
-      <View>
-        <Button
-          title="1"
-          onPress={() => this.kek(1)}/>
-        <Button
-          title="2"
-          onPress={() => this.kek(2)}/>
-        <Button
-          title="3"
-          onPress={() => this.kek(3)}/>
-      </View>
-      // <Icon
-      //   type="material-icons"
-      //   name="fingerprint"
-      //   size={66}
-      //   {...this.props.iconStyle}
-      //   onPress={this.handlePrint}
-      // />
+      // <View>
+      //   <Button
+      //     title="notification"
+      //     onPress={() => this.kek(1)}/>
+      //   <Button
+      //     title="impact"
+      //     onPress={() => this.kek(2)}/>
+      //   <Button
+      //     title="selection"
+      //     onPress={() => this.kek(3)}/>
+      // </View>
+      <Icon
+        type="material-icons"
+        name="fingerprint"
+        size={66}
+        {...this.props.iconStyle}
+        onPress={this.handlePrint}
+      />
     );
   }
 
   private kek = (type: number) => {
-    switch(type) {
-      case 1:
-        ReactNativeHaptic.generate('notification');
-        break;
-      case 2:
-        ReactNativeHaptic.generate('impact');
-        break;
-      case 3:
-        ReactNativeHaptic.generate('selection');
-        break;
-      default:
-        return null;
-    }
+    console.log(ReactNativeHaptic, 'bl')
+    // switch(type) {
+    //   case 1:
+    //     ReactNativeHaptic.generate('notification');
+    //     break;
+    //   case 2:
+    //     ReactNativeHaptic.generate('impact');
+    //     break;
+    //   case 3:
+    //     ReactNativeHaptic.generate('selection');
+    //     break;
+    //   default:
+    //     return null;
+    // }
   }
 
   private handlePrint = (): void => {
     console.log('there will be saga... probably');
     TouchID.authenticate().then((r: Object) => {
       if (Platform.OS === 'android') {
-        Vibration.vibrate(600, false);
+        Vibration.vibrate(100, false);
       } else {
         ReactNativeHaptic.generate('notification');
       }
